aace5556ef981e72fb29f8b2e2d6c7c0bb26211a
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 18:06:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,6 +22,7 @@ def apply_coupons(cart, coupons)
     if cart[item] && coupon[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
       cart[item][:count] -= coupon[:num]
+      elsif 
  end
 end
 
