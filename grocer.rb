005a98a7cc292c49b2bbc4c25da7a1e8ba75e30c
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 17:48:18 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,28 +18,8 @@ end
 
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
-    # add coupon to cart
-    item = coupon_hash[:item]
-
-    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
-      temp = {"#{item} W/COUPON" => {
-        :price => coupon_hash[:cost],
-        :clearance => hash[item][:clearance],
-        :count => 1
-        }
-      }
-      
-      if hash["#{item} W/COUPON"].nil?
-        hash.merge!(temp)
-      else
-        hash["#{item} W/COUPON"][:count] += 1
-        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
-      end
-      
-      hash[item][:count] -= coupon_hash[:num]
-    end
-  end
-  hash
+    
+ end
 end
 
 def apply_clearance(cart)
