e068923dbcf8d6304b5c183ed08feca575d1d62c
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 18:31:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,10 +33,7 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name, stats| 
-    if attribute_hash[:clearance] == true 
-      attribute_hash[:price] = (attribute_hash[:price] *
-      0.8).round(2) 
-    end 
+
   end 
 cart 
 end
