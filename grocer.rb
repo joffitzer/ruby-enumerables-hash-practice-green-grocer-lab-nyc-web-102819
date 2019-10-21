e807f7f4f8d53879184f8466e734aa10c7c040d1
Jonah Offitzer <joffitzer@gmail.com>
2019-10-21 18:44:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -42,4 +42,5 @@ def checkout(array, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
+  total = 
 end
